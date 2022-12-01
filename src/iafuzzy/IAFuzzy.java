b87cca73100d14ac5c55597529850c8896ccfa37
antonioaugusto1 <71223540+antonioaugusto1@users.noreply.github.com>
2022-12-01 08:14:41 -0300

    First Commit

---
+++ src/iafuzzy/IAFuzzy.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iafuzzy;
+
+/**
+ *
+ * @author 12626323667
+ */
+public class IAFuzzy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmFuzzy fuzzy = new FrmFuzzy();
+        fuzzy.setVisible(true);
+    }
+    
+}
